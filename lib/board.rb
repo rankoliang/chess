168c3a60b53736fa184181ae364846cff532b60a
Ranko Liang <rankoliang@gmail.com>
2020-07-09 20:21:56 -0400

    Refactor board background code
    Background code is decoupled from the to_s method to easily allow
    overriding of default colors.

--- lib/board.rb
+++ lib/board.rb
@@ -1,50 +1,27 @@
 # frozen_string_literal: true
 
 require_relative 'chess_config'
+require 'forwardable'
 
 # Contains information on the board state
 class Board
   attr_reader :board_array
+
+  extend Forwardable
+  include Enumerable
 
   def initialize
     # Initializes the board to be an array
     self.board_array = Array.new(ChessConfig::BOARD_HEIGHT) do
       Array.new(ChessConfig::BOARD_WIDTH)
     end
-  end
-
-  def [](index)
-    board_array[index]
-  end
-
-  def size
-    height = board_array.size
-    width = self[0].size
-    [width, height]
-  end
-
-  def self.chess_notation(column_index, row_index)
-    return if column_index >= ChessConfig::BOARD_WIDTH ||
-              row_index >= ChessConfig::BOARD_HEIGHT
-
-    column = (column_index + 'a'.ord).chr
-    "#{column}#{row_index + 1}"
-  end
-
-  # returns the chess piece at a certain position matching the format /[a..z]\d/
-  def at(position)
-    column = position[0].downcase
-    row = position[1]
-    column_index = column.ord - 'a'.ord
-    row_index = row.to_i - 1
-
-    self[row_index][column_index]
+    reset_background
   end
 
   def to_s
     # Draws from the bottom to up
     board_display = reverse.map.with_index do |row, row_index|
-      self.class.row_to_s(row_index, row)
+      row_to_s(row_index, row)
     end.join("\n")
 
     column_labels = ('a'..'z').to_a[0...ChessConfig::BOARD_WIDTH]
@@ -56,25 +33,60 @@ class Board
     puts to_s
   end
 
-  def to_ary
-    board_array
-  end
-
-  def self.row_to_s(row_index, row)
+  def row_to_s(row_index, row)
     row_of_pieces = row.map.with_index do |piece, column_index|
-      background_shade = (row_index + column_index) % 2
-
-      # Display an empty space by default
-      piece = ' ' if piece.nil?
-
-      # Draws the piece. Alternates between a blue and black background
-      "\e[#{40 + background_shade * 4}m #{piece} \e[0m"
+      Board.str_w_bg(piece || ' ', board_bg[row_index][column_index])
     end.join
 
     "#{ChessConfig::BOARD_HEIGHT - row_index} #{row_of_pieces}"
   end
 
+  def_delegators :board_array, :[], :reverse, :each, :to_ary, :each_index
+
+  def size
+    height = board_array.size
+    width = self[0].size
+    [width, height]
+  end
+
+  # returns the chess piece at a certain position matching the format /[a..z]\d/
+  def at(position)
+    column = position[0].downcase
+    row = position[1]
+    column_index = column.ord - 'a'.ord
+    row_index = row.to_i - 1
+
+    self[row_index][column_index]
+  end
+  class << self
+    def str_w_bg(string, bg_color)
+      color_code = ChessConfig::COLOR_CODES[bg_color]
+      "\e[#{color_code}m #{string} \e[0m"
+    end
+
+    def chess_notation(column_index, row_index)
+      return if column_index >= ChessConfig::BOARD_WIDTH ||
+                row_index >= ChessConfig::BOARD_HEIGHT
+
+      column = (column_index + 'a'.ord).chr
+      "#{column}#{row_index + 1}"
+    end
+  end
+
   private
 
   attr_writer :board_array
+  attr_accessor :board_bg
+
+  def reset_background
+    self.board_bg = board_array.map.with_index do |row, row_index|
+      self.class.row_background(row, row_index)
+    end
+  end
+
+  def self.row_background(row, row_index)
+    row.each_index.map do |column_index|
+      (row_index + column_index).even? ? :black : :blue
+    end
+  end
 end
--- lib/chess_config.rb
+++ lib/chess_config.rb
@@ -3,4 +3,5 @@
 module ChessConfig
   BOARD_WIDTH = 8
   BOARD_HEIGHT = 8
+  COLOR_CODES = { black: 40, blue: 44 }.freeze
 end
