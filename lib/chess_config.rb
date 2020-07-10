b61ecc3254d6df13ec587919d098d052f8be3dea
Ranko Liang <rankoliang@gmail.com>
2020-07-09 20:33:08 -0400

    Update COLOR_CODES

--- lib/chess_config.rb
+++ lib/chess_config.rb
@@ -3,5 +3,6 @@
 module ChessConfig
   BOARD_WIDTH = 8
   BOARD_HEIGHT = 8
-  COLOR_CODES = { black: 40, blue: 44 }.freeze
+  COLOR_CODES = { black: 40, blue: 44, red: 41, yellow: 42,
+                  orange: 43, magenta: 45, green: 46, white: 47 }.freeze
 end
