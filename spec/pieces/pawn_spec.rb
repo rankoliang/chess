8b3ed0f9928873e14d3b49929c45cd2af00e6e34
Ranko Liang <rankoliang@gmail.com>
2020-07-17 02:20:17 -0400

    tests: refactor and add pawn tests

--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../lib/board'
 
 RSpec.describe Pieces::Pawn do
   describe '#valid_moves' do
-    subject(:pawn) { described_class.new(position: position) }
+    subject(:pawn) { described_class.new(position: position, player: :white) }
 
     context 'when in starting position' do
       let(:position) { 'a2' }
@@ -15,38 +15,39 @@ RSpec.describe Pieces::Pawn do
       end
     end
 
-    context 'when able to capture a piece' do
-      let(:position) { 'a2' }
+    RSpec.shared_examples 'movable piece' do |subject_position, opponent_positions, valid_moves|
+      let(:position) { subject_position }
       let(:board) { Board.new }
       let(:defending_piece) { instance_double('Piece', player: :black) }
       let(:get_position) { proc { |position| board.at(position) } }
 
       before do
         allow(board).to receive(:at).and_call_original
-        allow(board).to receive(:at).with('b3').and_return(defending_piece)
+        opponent_positions.each do |opp_pos|
+          allow(board).to receive(:at).with(opp_pos).and_return(defending_piece)
+        end
       end
 
-      it 'can move up to two spaces forward or take a piece' do
+      it do
         expect(pawn.valid_moves(&get_position))
-          .to contain_exactly('a3', 'a4', 'b3')
+          .to contain_exactly(*valid_moves)
       end
     end
 
+    context 'when able to capture a piece' do
+      it_behaves_like 'movable piece', 'a2', ['b3'], %w[a3 a4 b3]
+    end
+
     context 'when blocked by an opposing piece' do
-      let(:position) { 'a2' }
-      let(:board) { Board.new }
-      let(:defending_piece) { instance_double('Piece', player: :black) }
-      let(:get_position) { proc { |position| board.at(position) } }
+      it_behaves_like 'movable piece', 'a2', ['a3'], []
+    end
 
-      before do
-        allow(board).to receive(:at).and_call_original
-        allow(board).to receive(:at).with('a3').and_return(defending_piece)
-      end
+    context 'when one space is blocked' do
+      it_behaves_like 'movable piece', 'a2', ['a4'], ['a3']
+    end
 
-      it 'can not move' do
-        expect(pawn.valid_moves(&get_position))
-          .to be_empty
-      end
+    context 'when blocked but able to capture a piece' do
+      it_behaves_like 'movable piece', 'a2', %w[b3 a4], %w[a3 b3]
     end
   end
 end
