fb9e04bfb09ee39d5f29ce98d1cdffd9f17446b3
Ranko Liang <rankoliang@gmail.com>
2020-07-19 18:26:45 -0400

    test: add integration script to test board display

---
+++ test_display.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'pry'
+require_relative 'lib/board'
+require_relative 'lib/chess_board'
+require_relative 'lib/piece'
+require_relative 'lib/chess_pieces'
+require_relative 'lib/chess'
+
+chess_game = Chess.new
+chess_game.board.draw
+chess_game.move('a1', 'a3')
+chess_game.board.draw
+chess_game.move('a1', 'a3')
+chess_game.board.draw
+
+# # puts board.pieces
+# # p ChessBoard.nested_hash_expand(ChessConfig::DEFAULT_LOCATIONS)
+# board.pieces.each do |location, piece|
+#   p [location, piece.to_s]
+# end.size
+# board.draw
+# board.pieces['a1'].move('a8')
+# board.draw
+# board.pieces.each do |location, piece|
+#   p [location, piece.to_s]
+# end.size
+# board.draw
+# board.pieces.each do |location, piece|
+#   p [location, piece.to_s]
+# end.size
+# # board.draw
